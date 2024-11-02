017ddc1cc7e45048566c856305a48c405d9cd46a
victoria-polyanovska <ipz242_pve@student.ztu.edu.ua>
2024-11-02 18:28:31 +0200

    Sana01

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,30 @@
+﻿double a, b, c, d;
+double x, y, z, r;
+
+Console.WriteLine("Enter a: ");
+string input = Console.ReadLine();
+a = double.Parse(input);
+
+Console.WriteLine("Enter b: ");
+input = Console.ReadLine();
+b = double.Parse(input);
+
+Console.WriteLine("Enter c: ");
+input = Console.ReadLine();
+c = double.Parse(input);
+
+Console.WriteLine("Enter d: ");
+input = Console.ReadLine();
+d = double.Parse(input);
+
+x = a + 2 * b - c + d / (c * d) + a + b / (c - d) - a * a / (b * b);
+Console.WriteLine($"x={x}");
+
+y = (5 * (a + b) * (c - d)) / (0.5 * c) + (d * d) * (a * a) - (b * b) / (b - a);
+Console.WriteLine($"y={y}");
+
+z = Math.Pow(x * x - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b);
+Console.WriteLine($"z={z}");
+
+r = (1 / 2) * a + (3 / 4) * b - (7 / 5) / (3 * c + 1) + 1 / (a - c);
+Console.WriteLine($"r={r}");
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,31 @@
+﻿double a, b, m, n; 
+double x, y, z1, z2;
+
+Console.WriteLine("Enter a: ");
+string input = Console.ReadLine();
+a = double.Parse(input);
+
+Console.WriteLine("Enter b: ");
+input = Console.ReadLine();
+b = double.Parse(input);
+
+Console.WriteLine("Enter m: ");
+input = Console.ReadLine();
+m = double.Parse(input);
+
+Console.WriteLine("Enter n: ");
+input = Console.ReadLine();
+n = double.Parse(input);
+
+Console.WriteLine("Enter x: ");
+input = Console.ReadLine();
+x = double.Parse(input);
+
+z1= ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / Math.Sqrt(Math.Pow(m, 3) * n + n * m + Math.Pow(m, 2) - m);
+Console.WriteLine($"z1={z1}");
+
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine($"z2={z2}");
+
+y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + 0.01 * (x - b);
+Console.WriteLine($"y={y}"); 
